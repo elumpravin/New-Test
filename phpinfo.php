641fcd382b0ae30077aefe951cd8840f6dc5575d
elumpravin <pravin_sawant@eluminoustechnologies.com>
2014-07-21 15:09:57 +0530

    Update phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document I am here 121313</title>
+<title>Untitled Document I am here 12</title>
 </head>
 
 <body>
